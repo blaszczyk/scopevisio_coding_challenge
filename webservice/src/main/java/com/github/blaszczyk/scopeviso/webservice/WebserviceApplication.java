e24214be8f2d12d158e8cbd18d53625f24319e6b
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2025-02-08 04:55:59 +0100

    RouterConfig

--- webservice/src/main/java/com/github/blaszczyk/scopeviso/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/github/blaszczyk/scopeviso/webservice/WebserviceApplication.java
@@ -1,34 +1,13 @@
 package com.github.blaszczyk.scopeviso.webservice;
 
-import com.github.blaszczyk.scopeviso.webservice.config.PostcodeGatewayConfiguration;
-import com.github.blaszczyk.scopeviso.webservice.config.PraemienGatewayConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator routes(final RouteLocatorBuilder builder,
-							   final PostcodeGatewayConfiguration postcodeGatewayConfig,
-							   final PraemienGatewayConfiguration praemienGatewayConfig
-							   ) {
-		return builder.routes()
-				.route(predicateSpec -> predicateSpec
-						.path(postcodeGatewayConfig.getPathPattern())
-						.uri(postcodeGatewayConfig.getUri()))
-				.route(predicateSpec -> predicateSpec
-						.path(praemienGatewayConfig.getPathPattern())
-						.uri(praemienGatewayConfig.getUri()))
-				.build();
 	}
 
 }
---
+++ webservice/src/main/java/com/github/blaszczyk/scopeviso/webservice/config/RouterConfig.java
@@ -0,0 +1,51 @@
+package com.github.blaszczyk.scopeviso.webservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouteLocator routes(final RouteLocatorBuilder builder,
+                               final PostcodeGatewayConfiguration postcodeGatewayConfig,
+                               final PraemienGatewayConfiguration praemienGatewayConfig
+    ) {
+        return builder.routes()
+                .route(predicateSpec -> predicateSpec
+                        .path(postcodeGatewayConfig.getPathPattern())
+                        .uri(postcodeGatewayConfig.getUri()))
+                .route(predicateSpec -> predicateSpec
+                        .path(praemienGatewayConfig.getPathPattern())
+                        .uri(praemienGatewayConfig.getUri()))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/ui/index.html") final Resource indexHtml) {
+        return route(GET("/ui/"), request -> ok()
+                .contentType(MediaType.TEXT_HTML)
+                .bodyValue(indexHtml)
+        );
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> redirectToUi() {
+        return route(GET("/"), req ->
+                ServerResponse.temporaryRedirect(URI.create("/ui/"))
+                        .build());
+    }
+}
